07ad710d3d9472b6553f03c4152d3db2ac9b5995
SauronKitty <evilmaniac@gmail.com>
2014-10-14 17:50:14 -0500

    Revert "Gradlestuff"
    This reverts commit f442d52aa5d947a4c02d90d0550543b2f6ed357e.
    
    Conflicts:
    	.gradle/2.0/taskArtifacts/cache.properties
    	.gradle/2.0/taskArtifacts/cache.properties.lock
    	.gradle/2.0/taskArtifacts/fileHashes.bin
    	.gradle/2.0/taskArtifacts/fileSnapshots.bin
    	.gradle/2.0/taskArtifacts/taskArtifacts.bin
    	.gradle/gradle.log

--- src/main/java/com/EssencePVP/PrimaryThread.java
+++ src/main/java/com/EssencePVP/PrimaryThread.java
@@ -11,21 +11,6 @@
 // You should have received a copy of the GNU General Public License
 // along with EssencePvP.  If not, see <http://www.gnu.org/licenses/>.
 
-// This file is part of EssencePvP.
-
-// EssencePvP is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// EssencePvP is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with EssencePvP.  If not, see <http://www.gnu.org/licenses/>.
-
 package com.EssencePVP;
 
 // Processes backend information
@@ -40,10 +25,10 @@ class PrimaryThread extends Thread {
 		load();
 		while (true){
 			try {
-				Thread.sleep(1000); // every second
+				Thread.sleep(1000 * 60);
 			} catch (InterruptedException e) {break;}
 			
-			// process background information and scheduling
+			// process background information
 		}
 	}
 	private void load(){ reload(); }
