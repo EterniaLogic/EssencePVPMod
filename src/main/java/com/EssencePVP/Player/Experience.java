a943fd9bfeb487393c39d0809887499ee5376c9f
EterniaLogic <eternialogic@gmail.com>
2014-10-14 12:21:08 -0500

    Fixed unbuildable

--- src/main/java/com/EssencePVP/Player/Experience.java
+++ src/main/java/com/EssencePVP/Player/Experience.java
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with EssencePvP.  If not, see <http://www.gnu.org/licenses/>.
 
-package com.EssencePVP;
+package com.EssencePVP.Player;
 
 // Uses event listener
 
