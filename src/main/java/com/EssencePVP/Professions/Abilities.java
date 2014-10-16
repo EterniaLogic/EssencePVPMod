d103db64563c3d962cc1a41ea3c14f82e31f0966
SauronKitty <evilmaniac@gmail.com>
2014-10-16 12:01:46 -0500

    Added comments to EssencePvP::Professions:Abilities class

--- src/main/java/com/EssencePVP/Professions/Abilities.java
+++ src/main/java/com/EssencePVP/Professions/Abilities.java
@@ -26,6 +26,10 @@ public class Abilities{
 		this.pHead = null;
 	}
 
+	// Description:
+	// Creates an Ability object and adds it to the head of an Abilities list given the Ability's Name and Description.
+	// Special note should be made as the pHead will always have the highest iAbilityId where as the tail of the list
+	// will have the smallest iAbilityId
 	public void addAbility(String _sAbilityName, String _sAbilityDescription){
 		if(iNumAbilities == 0)
 			pHead = new Ability(++iNumAbilities, _sAbilityName, _sAbilityDescription);
@@ -37,14 +41,21 @@ public class Abilities{
 		return;
 	}
 
+	// Description:
+	// Gets the ability by name, retreives its ID and then passes it to the delAbility(int _iAbilityId) function
 	public void delAbility(String _sAbilityName){
 		delAbility(getAbility(_sAbilityName).getAbilityId());
 	}
 
+	// Description:
+	// Passes the given _iAbilityId along with the head of the Abilities list to the delAbility(int _iAbility, 
+	// Ability _pAbility) function
 	public void delAbility(int _iAbilityId){
 		delAbility(_iAbilityId, this.pHead);
 	}
 
+	// Description:
+	// Recursivley scans an Ability list and removes an ability out of the Abilities list given an Ability's iAbilityID
 	private void delAbility(int _iAbilityId, Ability _pAbility){
 		if(_pAbility == null)
 			return;
@@ -97,6 +108,9 @@ public class Abilities{
 		return(this.iNumAbilities);
 	}
 
+	// Returns:
+	// false if the Abilities list is empty
+	// true otherwise
 	public boolean isAbilitiesEmpty(){
 		if(getAbilitiesCount() > 0)
 			return false;
