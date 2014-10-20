7829ff79b36badbb0d08998cea2ac33fafd6ca48
Brent Clancy <dreadslicer@gmail.com>
2014-10-19 22:21:13 -0500

    ibid!!!!

--- src/main/java/com/EssencePVP/EssencePVP.java
+++ src/main/java/com/EssencePVP/EssencePVP.java
@@ -142,10 +142,6 @@ public class EssencePVP
 	public void RegisterCommands() {
 		// Register Commands
     	commandManager = (ServerCommandManager) Minecraft.getMinecraft().getIntegratedServer().getCommandManager();
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
     	commandManager.registerCommand(new testKillExp()); // "/test"
     	commandManager.registerCommand(new cmdAddProfession()); // addProfession Name Description
 	}
