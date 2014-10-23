22f5fe65193c70769ae9ef91b7ab70badc74308a
James Little <wizkid916@yahoo.com>
2014-10-23 00:29:24 -0500

    finished the load method for ProfessionsManager

--- src/main/scala/com/EssencePVP/DatabaseManger/ProfessionsManager.scala
+++ src/main/scala/com/EssencePVP/DatabaseManger/ProfessionsManager.scala
@@ -7,7 +7,7 @@ object ProfessionsManager {
   def load : Professions.Professions = {
     val professionList = new Professions.Professions
     models.Professions.retrieveAll().foreach(element => { //iterate through element in the DB and add it to the list
-      professionList.addProfession(element.name, element.description) //add to the list
+      professionList.addProfession(element.id, element.name, element.description) //add to the list
     })
     professionList //return the list
   }
