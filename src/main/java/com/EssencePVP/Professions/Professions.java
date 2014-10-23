028aab381a60b50b0f933be6b4a9d8418f289017
evilmaniac <evilmaniac@gmail.com>
2014-10-23 00:50:53 -0500

    Removed iNumProfessionsAddedCounter. The addProfession function will now extract the id of the last added profession and increment it by 1 when adding; as opposed to using a counter

--- src/main/java/com/EssencePVP/Professions/Professions.java
+++ src/main/java/com/EssencePVP/Professions/Professions.java
@@ -17,16 +17,13 @@
 
 package com.EssencePVP.Professions;
 
-public class Professions implements java.io.Serializable
-{
+public class Professions implements java.io.Serializable {
 	private int iNumProfessions; // This is the current number of professions in the list
-	private int iNumProfessionsAdded; // This is the total addProfession() calls that have been made
 	private Profession pHead;
 	private Profession pLast;
 
 	public Professions(){
 		this.iNumProfessions = 0;
-		this.iNumProfessionsAdded = 0;
 		this.pHead = null;
 		this.pLast = null;
 	}
@@ -38,10 +35,10 @@ public class Professions implements java.io.Serializable
 	// - AK
 	public Profession addProfession(String _sProfessionName, String _sProfessionDescription){
 		if(iNumProfessions == 0)
-			pHead = new Profession(++iNumProfessionsAdded, _sProfessionName, _sProfessionDescription);
+			pHead = new Profession(1, _sProfessionName, _sProfessionDescription);
 		else{
 			Profession pTemporary = pHead;
-			pHead = new Profession(++iNumProfessionsAdded, _sProfessionName, _sProfessionDescription);
+			pHead = new Profession((pTemporary.getProfessionId()+1), _sProfessionName, _sProfessionDescription);
 			pHead.setNext(pTemporary);
 		}
 		++iNumProfessions;
@@ -50,7 +47,7 @@ public class Professions implements java.io.Serializable
 
 	public Profession addProfession(int _iProfessionId, String _sProfessionName, String _sProfessionDescription){
 		if(iNumProfessions == 0)
-			if(_iProfessionId > iNumProfessions)
+			if(_iProfessionId > 0)
 				pHead = new Profession(_iProfessionId, _sProfessionName, _sProfessionDescription);
 			else return null;
 		else{
@@ -60,7 +57,6 @@ public class Professions implements java.io.Serializable
 				pHead.setNext(pTemporary);
 			} else return null;
 		}
-		++iNumProfessionsAdded;
 		++iNumProfessions;
 		return(pHead);
 	}
