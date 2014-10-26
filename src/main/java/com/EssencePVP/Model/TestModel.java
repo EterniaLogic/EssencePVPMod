7ee0c1e8795b4d3474201d8e2429769a96395a3e
Brent Clancy <dreadslicer@gmail.com>
2014-10-26 04:26:38 -0500

    added some functions

--- src/main/java/com/EssencePVP/Model/TestModel.java
+++ src/main/java/com/EssencePVP/Model/TestModel.java
@@ -1,5 +1,19 @@
 package com.EssencePVP.Model;
 
+import com.sun.xml.internal.stream.Entity;
+
 public class TestModel extends Entity {
+
+	@Override
+	public boolean isExternal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isUnparsed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
