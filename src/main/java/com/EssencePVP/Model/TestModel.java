2b4ab3758787c5550f25a90da311188184517263
Brent Clancy <dreadslicer@gmail.com>
2014-10-29 11:05:47 -0500

    fixed testmodel

--- src/main/java/com/EssencePVP/Model/TestModel.java
+++ src/main/java/com/EssencePVP/Model/TestModel.java
@@ -1,19 +1,7 @@
 package com.EssencePVP.Model;
 
-import com.sun.xml.internal.stream.Entity;
 
-public class TestModel extends Entity {
+public class TestModel/* extends Entity*/ {
 
-	@Override
-	public boolean isExternal() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isUnparsed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 }
