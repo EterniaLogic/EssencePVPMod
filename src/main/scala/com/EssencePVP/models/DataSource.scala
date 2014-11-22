74e3e2d737cfff5fcc136960b95a42a18442445f
James Little <wizkid916@yahoo.com>
2014-11-22 10:07:28 -0600

    Cleaned up DataSource Code. Added Comments.

--- src/main/scala/com/EssencePVP/models/DataSource.scala
+++ src/main/scala/com/EssencePVP/models/DataSource.scala
@@ -1,21 +1,19 @@
 package com.EssencePVP.models
 
-
-import java.io.File
-
 import com.jolbox.bonecp.BoneCPDataSource
-import scala.slick.driver.MySQLDriver
 import scala.slick.driver.MySQLDriver.simple._
 import com.typesafe.config.ConfigFactory
 import scala.util.Try
 
 
 //Creates a Connection to the DB using the settings found in application.conf
-
+//This will try to use MySQL first, then will fail over to SQLite
 
 object DataSource {
+
   val config = ConfigFactory.load()
   val ds = new BoneCPDataSource()
+
   ds.setJdbcUrl(config.getString("Database.Config.main.url"))
   ds.setUsername(config.getString("Database.Config.main.user"))
   ds.setPassword(config.getString("Database.Config.main.password"))
@@ -27,13 +25,18 @@ object DataSource {
   println("[Data Source]: Trying to use MySQL Database...")
 
   Try {
+
     ds.getConnection
     println("[Data Source]: Connected to MySQL Database.")
+
   } getOrElse {
+
     println("[Data Source]: Failed to connect to MySQL Database.")
     println("[Data Source]: Using SQLite Instead...")
+
     ds.setJdbcUrl(config.getString("Database.Config.local.url"))
     ds.setDriverClass("org.sqlite.JDBC")
+
   }
 
   var DB = Database.forDataSource(ds)
