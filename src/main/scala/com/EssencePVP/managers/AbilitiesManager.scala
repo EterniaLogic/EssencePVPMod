7a87bc432746dcf4c10f3182a96d1be62a9d48de
James Little <wizkid916@yahoo.com>
2014-11-23 13:59:12 -0600

    Made AbilitiesManager more "Readable".

--- src/main/scala/com/EssencePVP/managers/AbilitiesManager.scala
+++ src/main/scala/com/EssencePVP/managers/AbilitiesManager.scala
@@ -1,13 +1,18 @@
 package com.EssencePVP.managers
 
-import com.EssencePVP._
+import com.EssencePVP.Professions.Abilities
+import com.EssencePVP.Professions.Ability
+import com.EssencePVP.models.AbilityProperties
+import com.EssencePVP.models.{ Abilities => AbilitiesDB }
+import com.EssencePVP.models.{ Ability => AbilityModel }
+
 import scala.util.Try
 
 object AbilitiesManager {
 
-  def load : Professions.Abilities = { //load from DB into the list
-    def loadAbilityProperties(abilityList: Professions.Abilities): Professions.Abilities = { //load from the AbilityProperty table
-      models.AbilityProperties.retrieveAll.foreach(element => {
+  def load : Abilities = { //load from DB into the list
+    def loadAbilityProperties(abilityList: Abilities): Abilities = { //load from the AbilityProperty table
+      AbilityProperties.retrieveAll.foreach(element => {
         val ability = Try(abilityList.getAbility(element.ability)) //get by the ability id
         if(ability.isSuccess) { //if that exists, then add the property to that ability.
           ability.get.addAbilityProperty(
@@ -21,48 +26,52 @@ object AbilitiesManager {
       abilityList
     }
 
-    val abilityList = new Professions.Abilities
+    val abilityList = new Abilities
 
-    models.Abilities.retrieveAll().foreach(element => { //look at ea. element in the DB and add it to the list
+    AbilitiesDB.retrieveAll().foreach(element => { //look at ea. element in the DB and add it to the list
       abilityList.addAbility(element.id, element.name, element.description) //add to the list
     })
     loadAbilityProperties(abilityList)
   }
-  def add(property:Int, name:String, description:String, abilityList:Professions.Abilities) : Professions.Ability  = {
+  def add(property:Int, name:String, description:String, abilityList:Abilities) : Ability  = {
     val newAbility = abilityList.addAbility(name, description) //add to the list
-    models.Abilities.create(models.Ability(newAbility.getAbilityId, "", name, description)) //add to the DB
+    AbilitiesDB.create(AbilityModel(newAbility.getAbilityId, "", name, description)) //add to the DB
     newAbility //return the newAbility
   }
   def setIcon(id:Int, icon:String) = {
-    val ability = models.Abilities.retrieve(id)
-    models.Abilities.update(models.Ability(
-      ability.id,
-      icon,
-      ability.description,
-      ability.name
-    ))
+    val ability = AbilitiesDB.retrieve(id)
+    AbilitiesDB.update(
+      AbilityModel(
+        ability.id,
+        icon,
+        ability.description,
+        ability.name
+      )
+    )
   }
   def setIcon(name:String, icon:String) = {
-    val ability = models.Abilities.retrieve(name)
-    models.Abilities.update(models.Ability(
-      ability.id,
-      icon,
-      ability.description,
-      ability.name
-    ))
+    val ability = AbilitiesDB.retrieve(name)
+    AbilitiesDB.update(
+      AbilityModel(
+        ability.id,
+        icon,
+        ability.description,
+        ability.name
+      )
+    )
   }
-  def get(id:Int, abilityList:Professions.Abilities) : Professions.Ability = {
+  def get(id:Int, abilityList:Abilities) : Ability = {
     abilityList.getAbility(id) //get the ability from the list by id
   }
-  def get(name:String, abilityList:Professions.Abilities) : Professions.Ability = {
+  def get(name:String, abilityList:Abilities) : Ability = {
     abilityList.getAbility(name) //get the ability from the list by name
   }
-  def del(id:Int, abilityList:Professions.Abilities) : Unit = {
-    models.Abilities.delete(id) //delete from DB by id
+  def del(id:Int, abilityList:Abilities) : Unit = {
+    AbilitiesDB.delete(id) //delete from DB by id
     abilityList.delAbility(id) //delete from the list by id
   }
-  def del(name:String, abilityList:Professions.Abilities) : Unit = {
-    models.Abilities.delete(name) //delete from DB by name
+  def del(name:String, abilityList:Abilities) : Unit = {
+    AbilitiesDB.delete(name) //delete from DB by name
     abilityList.delAbility(name) //delete from list by name
   }
 }
