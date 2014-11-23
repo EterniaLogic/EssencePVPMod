e0bca4ebbb60f5025a09cac3ad02b12da4d35d4d
James Little <wizkid916@yahoo.com>
2014-11-23 14:22:44 -0600

    Added length\sizes to CharactersManager.

--- src/main/scala/com/EssencePVP/managers/CharactersManager.scala
+++ src/main/scala/com/EssencePVP/managers/CharactersManager.scala
@@ -6,12 +6,16 @@ import com.EssencePVP.character.Character
 object CharactersManager {
   private var list:Vector[Character] = Vector()
 
-  def add(c:Character) = list = list :+ c
+  def add(c:Character) = list =  list :+ c
 
   def delete(c:Character) = list = list.filter(_ != c)
 
   def delete(playerName:String) = list = list.filter(_.getName != playerName)
 
   def getList : Vector[Character] = list
+
+  def size : Int = list.size
+
+  def length : Int = list.size //why not have both (size and length)? :)
 
 }
