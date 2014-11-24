45a55c531adb00f5c2ab708250946499365c7f4e
Brent Clancy <dreadslicer@gmail.com>
2014-11-24 17:37:34 -0600

    Added implementation for lightning command

--- src/main/java/com/EssencePVP/Command/LightningCommand.java
+++ src/main/java/com/EssencePVP/Command/LightningCommand.java
@@ -1,8 +1,15 @@
 package com.EssencePVP.Command;
 
+import com.EssencePVP.Player.Player;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.MovingObjectPosition.MovingObjectType;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 
 public class LightningCommand extends CommandBase{
 
@@ -24,5 +31,19 @@ public class LightningCommand extends CommandBase{
 	public void processCommand(ICommandSender icommandsender, String[] astring)
 	{
 	     // Still empty for now
+		Minecraft mc = Minecraft.getMinecraft();
+		
+        if (mc != null && mc.objectMouseOver != null) {
+        	Player player = (Player) Player.getPlayerMap().get(icommandsender.getCommandSenderName());
+        	
+        	if(player != null){
+        		World world = player.getMcplayer().worldObj;
+        		Vec3 lookat = player.getMcplayer().getLookVec();
+        		
+        		EntityLightningBolt lightningbolt = new EntityLightningBolt(world, lookat.xCoord, 128, lookat.yCoord);
+
+	            world.spawnEntityInWorld(lightningbolt);
+        	}
+        }
 	}
 }
